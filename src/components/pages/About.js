1de05eac656305211e0799727f3a0fdaa572ea9f
Kostas Bourantas <bourantas@ceid.upatras.gr>
2020-04-05 20:40:27 +0300

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="container" style={{padding:'10px'}}>
+      <h1>About</h1>
+      <p>
+          This is the TodoList app v1.0.0. 
+          It is part of a <a href="https://www.youtube.com/watch?v=sBws8MSXN7A&t=4977s" style={{color:'blue'}}>React crash course</a>.
+      </p>
+    </div>
+  );
+}
